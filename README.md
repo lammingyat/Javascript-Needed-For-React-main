065e59b516049acdcd9599eb912a043331630dcd
lammingyat <lammingyatca@gmail.com>
2023-07-31 15:54:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# JavaScript Mini-Projects
+
+This repository contains a series of JavaScript mini-projects. Each project focuses on a specific concept in JavaScript and provides a hands-on way to learn and practice these concepts.
+
+## Getting Started
+1. Clone the repository to your local machine.
+2. Navigate to the project you wish to start with. 
+3. The tasks are outlined as comments in the JavaScript file. Replace each comment with your code solution.
+4. Use the command `npm run watch` to start nodemon and watch for changes in your JavaScript file.
+
+## Project 1: Function Conversion
+Convert regular JavaScript functions into arrow functions. 
+
+## Project 2: Conditional Rendering
+Create a simple web page where the content changes based on a variable's value, using a ternary operator.
+
+## Project 3: User Profiles
+Create an array of user profile objects, each containing properties such as name, age, email, etc.
+
+## Project 4: Shopping Cart
+Create a mock shopping cart. Items can be added to the cart, displayed, filtered by various properties, and the total price calculated.
+
+## Project 5: Simple Weather App
+Fetch data from a weather API and display it on a webpage.
+
+## Resources
+- [General JavaScript tutorial](https://www.youtube.com/watch?v=m55PTVUrlnA)
+- [Guide on fetching data from an API in JavaScript](https://www.youtube.com/watch?v=SbCedTlJWTs)
+
+## Contribution
+Contributions, issues, and feature requests are welcome. Feel free to check [issues page](../../issues) if you want to contribute.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ script.js
@@ -0,0 +1,86 @@
+// Resource: https://www.youtube.com/watch?v=m55PTVUrlnA
+// Fetching data Resource: https://www.youtube.com/watch?v=SbCedTlJWTs
+// Project 1: Function Conversion
+/* 
+Objective: To practice and understand the usage of arrow functions in JavaScript.
+Challenge: You will be given a JavaScript file containing several regular functions. Your task is to convert these functions into arrow functions.
+Requirements: Preserve the original functionality of the code while converting. Verify your conversion by testing the output of each function.
+Difficulty: Beginner
+*/
+
+// Here are functions to convert:
+
+function greeting(name) {
+  return "Hello, " + name + "!";
+}
+
+// A function that squares a number
+function square(num) {
+  return num * num;
+}
+
+// A function that takes two numbers and returns their sum
+function add(a, b) {
+  return a + b;
+}
+
+// A function that takes a number and returns true if the number is even, false otherwise
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+// A function that takes a string and returns its reverse
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+// End of Project 1
+
+// Project 2: Conditional Rendering
+/* 
+Objective: To understand the use of ternary operators for conditional rendering in JavaScript.
+Challenge: Create a simple web page where the content changes based on a variable's value, using a ternary operator.
+Requirements: The variable can be of any type. The webpage should display different content based on at least three different states of the variable.
+Difficulty: Beginner/Intermediate
+*/
+
+
+// Write your code here.
+
+// End of Project 2
+
+// Project 3: User Profiles
+/* 
+Objective: To learn how to create, access, and modify JavaScript objects.
+Challenge: Create an array of user profile objects, each containing properties such as name, age, email, etc.
+Requirements: Each user profile object should have at least five properties. Console log all user profiles. Console log a specific property of a user.
+Difficulty: Beginner
+*/
+
+// Write your code here.
+
+// End of Project 3
+
+// Project 4: Shopping Cart
+/* 
+Objective: To practice the use of array methods in JavaScript.
+Challenge: Create a mock shopping cart. Items can be added to the cart, displayed, filtered by various properties, and the total price calculated.
+Requirements: The shopping cart should be implemented as an array of objects, each representing an item. Implement functionality to add items, display the list of items, filter the items based on properties (e.g., category, price), and calculate the total price.
+Difficulty: Intermediate
+*/
+
+// Write your code here.
+
+// End of Project 4
+
+// Project 5: Simple Weather App
+/* 
+Objective: To learn how to use async-await, promises, and fetch in JavaScript.
+Challenge: Fetch data from a weather API and display it on a webpage.
+Requirements: Use the fetch API to fetch weather data from a public weather API. Handle the promise returned by fetch using async-await. Display the fetched data on a webpage in a human-readable format.
+Difficulty: Intermediate/Advanced
+*/
+
+// Write your code here.
+
+// End of Project 5
